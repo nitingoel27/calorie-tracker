9d82cbceb2ed63fa5606d54669a5b90b4a72b98b
nitingoel27 <guptanitin3333@gmail.com>
2025-12-18 09:26:33 +0530

    PWA-SETUP

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+
 import { CalorieProvider } from "./context/CalorieContext"
+import { registerSW } from "virtual:pwa-register"
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
